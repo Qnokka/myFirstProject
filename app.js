f32809353bae4445c98f52a90a196529411e5034
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-06-13 18:57:16 +0900

    Add preventDefault() prevents basic operations from being performed

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function handleLoginSubmit(event) {
     //console.dir(loginInput); //input의 내용을 가져오기 위한 property 확인
     event.preventDefault();
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", handleLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
+//handleLinkClick({Information about the event that just happened});
+//방금 일어난 event에 대한 정보를 담은 object를 JS가 기본적으로 제공하고, 첫번째 인자로 주어짐
