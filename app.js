60ab0065cd26e83a20f1b38913b83406435c2fa4
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-06-17 19:35:44 +0900

    3.6 Loading Username

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; 
 //중요한 정보를 담은 게 아닌 경우 대문자로 작성
+const USERNAME_KEY = "username";
+//무조건 같아야 하는 string은 대문자 변수로 저장
 
 function handleLoginSubmit(event) {
     //console.dir(loginInput); //input의 내용을 가져오기 위한 property 확인
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     //username을 DB에 저장해주는 구문
     //localStorage. setItem/getItem/removeItem을 통해 DB 작업
-    greeting.innerText = `Hello, ${username} Keep Going`;
+    paintGrettings(username);
+}
+
+function paintGrettings(username) {
+    greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", handleLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleLoginSubmit);
+} else {
+    //show the greetings
+    paintGrettings(savedUsername);
+}
