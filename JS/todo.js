9bc61c8bbc587457fd3b698e0fcc3fd903f8839f
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-06-21 23:25:01 +0900

    6.2 Deleting ToDos

--- JS/todo.js
+++ JS/todo.js
@@ -3,13 +3,21 @@ const toDoInput = toDoForm.querySelector("input");
 //document.getElementById("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDO(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const btn = document.createElement("button");
+    btn.innerText = "❌";
+    btn.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
     toDoList.appendChild(li);
-    //JS에서 HTML 생성하는 방식
 }
 
 function handleToDoSubmit(event) {
